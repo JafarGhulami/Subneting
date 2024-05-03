82dde6d44ebf9d5144cca1269d0a6422eeadeea1
Jafar Ghulami <jafarghulami@gmail.com>
2024-05-03 13:19:02 +0430

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 from numpy import random
 
-x = random.randint(100)
-print(x)
 
-#print float num from 0 to 1
-y = random.rand()
-print("this is a fload number", y)
+my_arr = random.randint(100, size = 10)
+
+print(my_arr)
